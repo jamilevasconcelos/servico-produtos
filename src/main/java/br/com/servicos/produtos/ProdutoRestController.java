1d29db86ec903a78b2c1d171cc264a38f2e298d6
Jamile <jamilevasconcelos@gmail.com>
2019-10-24 20:44:36 -0300

    post lista de compras

--- src/main/java/br/com/servicos/produtos/ProdutoRestController.java
+++ src/main/java/br/com/servicos/produtos/ProdutoRestController.java
@@ -1,12 +1,20 @@
 package br.com.servicos.produtos;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URL;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.omg.CORBA.portable.OutputStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -58,18 +66,38 @@ public class ProdutoRestController {
 
 	// checar se quantidade do produto está abaixo da quantidade minima
 	@GetMapping("/api/produtos/checarsequantidademinima")
-	public  List<Produto> checarSeQuantidadeMinima() {
+	public void checarSeQuantidadeMinima() throws IOException {
 		
 		List<Produto> todosprodutos = new ArrayList<Produto>(produtoRepository.findAll());		
-		List<Produto> comprasnecessarias = new ArrayList<Produto>();
-				
+
+		URL url = new URL("https://compra.free.beeceptor.com");
+
 		for (int i = 0; i < todosprodutos.size(); i++) {
 			Produto produto = todosprodutos.get(i);
 			if  (produto.getQtdd() <= produto.getMinima()) {
-				comprasnecessarias.add(produto);
+			
+				HttpURLConnection con = (HttpURLConnection) url.openConnection();
+				con.setRequestMethod("POST");
+				con.setRequestProperty("Content-Type", "application/json; utf-8");
+				con.setRequestProperty("Accept", "application/json");
+				con.setDoOutput(true);
+				
+				String jsonInputString = "{'datahora': " + LocalDateTime.now() + " 'produto': " 
+										 + produto.getId() + ", 'fornecedor': " 
+										 + produto.getFornecedor() + " '}";
+				
+				try(BufferedReader br = new BufferedReader(
+				  new InputStreamReader(con.getInputStream(), "utf-8"))) {
+				    StringBuilder response = new StringBuilder();
+				    String responseLine = null;
+				    while ((responseLine = br.readLine()) != null) {
+				        response.append(responseLine.trim());
+				    }
+				    
+				    System.out.println(response.toString());
+				}
 			}
 		}	
-		return comprasnecessarias;		
 	}
 	
 	
